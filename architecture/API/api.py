8be1b77e3f992fbe00990e89dd021eda2149e829
Michele Spina <michelespina96@gmail.com>
2023-05-04 13:54:34 +0200

    comment code

--- architecture/API/api.py
+++ architecture/API/api.py
@@ -9,7 +9,21 @@ from CAKEDataOwner import CAKEDataOwner
 
 app = Flask(__name__)
 
-def getClientArgs(request):
+""" Read the arguments from the client request
+
+    This function is used to get the arguments from the client request
+    and return them in the correct format to be used by the CAKEClient
+
+    Args:
+        request: the request from the client
+    
+    Returns:
+        reader_address: the address of the reader
+        message_id: the id of the message
+        slice_id: the id of the slice
+        process_id: the id of the process (process_instance_id)
+"""
+def __get_client_args__(request):
     process_id = request.json.get('process_id')
     reader_address = request.json.get('reader_address')
     message_id = request.json.get('message_id')
@@ -22,17 +36,39 @@ def getClientArgs(request):
     print("Process_id is: " + str(process_id))
     return reader_address, message_id, slice_id, process_id
 
-def getDataOwnerArgs(request):
-    return
 
+""" A simple request to the API welcome message
+
+    This function is used to test if the API is working correctly
+    during the development phase
+
+    Returns:
+        A welcome message
+"""
 @app.route('/')
 def go_home():
     return 'Welcome to the CAKE API'
 
 #### Request from client to SKM Server ####
+
+""" Request to the SKM Server to handshaking
+
+    This function is used to send a request to the SKM Server
+    to make an handshake with the reader
+ 
+    Args:
+        reader_address: the address of the reader
+        message_id: the id of the message
+        slice_id: the id of the slice
+
+    Returns:
+        The status of the request, 200 if the handshake is completed
+"""
+
+
 @app.route('/client/handshake/' , methods=['GET', 'POST'])
 def client_handshake():
-    reader_address, message_id, _, process_id = getClientArgs(request)
+    reader_address, message_id, _, process_id = __get_client_args__(request)
     if reader_address == '' or message_id == '':
         return "Missing parameters" , 400   
     client = CAKEClient(message_id=message_id, reader_address=reader_address, process_instance_id=process_id)
@@ -40,31 +76,72 @@ def client_handshake():
     #client.disconnect()
     return "Handshake completed" , 200
 
+
+""" Request to the SKM Server to generate a key
+
+    This function is used to send a request to the SKM Server
+    to generate a key for the reader.
+    The key is generated only if the handshake is completed.
+    
+    Args:
+        reader_address: the address of the reader
+        message_id: the id of the message
+        slice_id: the id of the slice
+        process_id: the id of the process (process_instance_id)
+        
+    Returns:
+        The status of the request, 200 if the key is generated
+"""
 @app.route('/client/generateKey/' , methods=['GET', 'POST'])
 def generateKey():
-    reader_address, message_id, _, process_id = getClientArgs(request)
+    reader_address, message_id, _, process_id = __get_client_args__(request)
     if reader_address == '' or message_id == '':
         print("Missing parameters")
         return "Missing parameters" , 400
     client = CAKEClient(message_id=message_id, reader_address=reader_address, process_instance_id = process_id)
     client.generate_key()
-    return "Key generated"
+    return "Key generated", 200
 
+""" Request to the SKM Server to access data
+
+    This function is used to send a request to the SKM Server
+    to access data from the reader.
+    The data is accessed only if the reader already has the key.
+    
+
+    Returns: 
+        The status of the request, 200 if the data is accessed
+"""
 @app.route('/client/accessData/' , methods=['GET', 'POST'])
 def accessData():
-    reader_address, message_id, slice_id, process_id = getClientArgs(request)
+    reader_address, message_id, slice_id, process_id = __get_client_args__(request)
     if reader_address == '' or message_id == '' or slice_id == '':
         return "Missing parameters" , 400
     client = CAKEClient(message_id=message_id, reader_address=reader_address, slice_id=slice_id, process_instance_id= process_id)
     client.access_data()
     #client.disconnect()   
 
-    return "Data accessed"
+    return "Data accessed", 200
 
 # This is a full request, it does handshake, generate key and access data
+""" Request to the SKM Server to send a request of handshake, generate key and access data
+
+    This function is used to send a request to the SKM Server
+    to make an handshake, generate a key and access data.
+    
+    Args:
+        reader_address: the address of the reader
+        message_id: the id of the message
+        slice_id: the id of the slice
+        process_id: the id of the process (process_instance_id)
+        
+    Returns:
+        The status of the request, 200 if all the operations are completed
+"""
+
 @app.route('/client/fullrequest/' , methods=['GET', 'POST'])
 def client_fullrequest():
-    reader_address, message_id, slice_id, process_id = getClientArgs(request)
+    reader_address, message_id, slice_id, process_id = __get_client_args__(request)
     if reader_address == '' or message_id == '' or slice_id == '':
         return "Missing parameters" , 400
     client = CAKEClient(message_id=message_id, reader_address=reader_address, slice_id=slice_id, process_instance_id= process_id)
@@ -76,16 +153,43 @@ def client_fullrequest():
     client = CAKEClient(message_id=message_id, reader_address=reader_address, slice_id=slice_id, process_instance_id= process_id)
     client.access_data()
     print("Data access launched")
-    return "Handshake completed"
+    return "Handshake completed", 200
 
 ##### Request from Data Owner to SDM Server #####
+""" Request to the SDM Server to handshaking
 
+    This function is used to send a request to the SDM Server
+    to make an handshake with the MANUFACTURER
+
+    Args:
+        process_id: the id of the process (process_instance_id)
+    
+    Returns:
+        The status of the request, 200 if the handshake is completed
+    """
 @app.route('/dataOwner/handshake/' , methods=['POST'])
 def data_owner_handshake():
     data_owner = CAKEDataOwner(process_instance_id=request.json.get('process_id'))
     data_owner.handshake()
-    return "Handshake completed"
+    return "Handshake completed", 200
 
+""" Request to the SDM Server to cipher the message from the Manufacturer
+
+    This function is used to send a request to the SDM Server
+    to cipher the message from the Manufacturer, setting the policy
+    of the decryption.
+
+    Args:
+        message: the message to cipher, it's a string read from a json file
+        entries:  a list of list of label of the message that has the same policy
+        policy: a list containing for each group of label the process_id associated
+            and the policy, defining which actors can access the data
+
+    Returns:
+        The status of the request, 200 if the cipher is completed
+
+
+"""
 @app.route('/dataOwner/cipher/', methods=['POST'])
 def cipher():
     message = request.json.get('message')
@@ -111,8 +215,24 @@ def cipher():
 
     data_owner = CAKEDataOwner(process_instance_id=request.json.get('process_id'))
     data_owner.cipher_data(message, entries_string, policy_string)
-    return "Cipher completed"
+    return "Cipher completed", 200
 
+
+""" Request to the SDM Server to send a request of handshake, cipher  the message
+
+    This function is used to send a request to the SDM Server
+    to make an handshake, cipher the message and send it to the MANUFACTURER
+    
+    Args:
+        message: the message to cipher, it's a string read from a json file
+        entries:  a list of list of label of the message that has the same policy
+        policy: a list containing for each group of label the process_id associated
+            and the policy, defining which actors can access the data
+        process_id: the id of the process (process_instance_id)
+        
+    Returns:   
+        The status of the request, 200 if all the operations are completed
+"""
 @app.route('/dataOwner/fullrequest/', methods=['POST'])
 def dataowner_fullrequest():
     message = request.json.get('message')
@@ -136,7 +256,7 @@ def dataowner_fullrequest():
     data_owner.handshake()
     data_owner = CAKEDataOwner(process_instance_id=request.json.get('process_id'))
     data_owner.cipher_data(message, entries_string, policy_string)
-    return "Full request completed"
+    return "Full request completed", 200
 
 @app.route('/certification/', methods=['POST'])
 def certification():
@@ -145,18 +265,49 @@ def certification():
     Certifier.certify(actors, roles)
     return "Certification done"
 
+""" Read the public keys of the actors
+
+    This function is used to read the public keys of the actors
+    that are involved in the process
+    
+    Args:
+        actors: the list of actors involved in the process
+        roles: a dictionary that contains for each actor the list of roles associated
+
+    Returns:
+        The status of the request, 200 if the keys are read correctly
+"""
 @app.route('/certification/readpublickey/', methods=['POST'])
 def read_public_key():
     actors = request.json.get('actors')
     #roles = request.json.get('roles')
     Certifier.read_public_key(actors)
-    return "Public keys read"
+    return "Public keys read", 200
 
+""" Read the public key of the SKM
+
+    This function is used to read the public key of the SKM
+
+    Returns:
+        The status of the request, 200 if the keys are read correctly
+"""
 @app.route('/certification/skmpublickey/', methods=['GET', 'POST'])
 def skm_public_key():
     Certifier.skm_public_key()
-    return "SKM public key read"
+    return "SKM public key read", 200
 
+""" Certificate the actors
+
+    This function is used to certificate the actors
+    that are involved in the process
+    
+    Args:
+        actors: the list of actors involved in the process
+        roles: a dictionary that contains for each actor the list of roles associated
+        
+    Returns: 
+        The status of the request, 200 if the actors are certified correctly
+"""
 @app.route('/certification/attributecertification/', methods=['POST'])
 def attribute_certification():
     #actors = request.json.get('actors')
@@ -164,10 +315,18 @@ def attribute_certification():
     Certifier.attribute_certification(roles)
     return "Attribute certification done"
 
+""" Test function
+
+    This function is used to test the server during the development phase.
+
+    Returns:
+        A string that says "Test done"
+"""
 @app.route('/test/', methods=['GET', 'POST'])
 def test():
     import os
     os.system("ls")
     return "Test done"
+
 if __name__ == '__main__':
-    app.run(port=8889)
+    app.run(port=8888)
