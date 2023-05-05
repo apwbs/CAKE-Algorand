77527b74594b6cada2409e27dea09e7127458b7b
Michele Spina <michelespina96@gmail.com>
2023-05-05 16:18:06 +0200

    add samples of api

---
+++ architecture/API/samples/api_client.py
@@ -0,0 +1,37 @@
+import requests
+import argparse
+from decouple import config
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-hs', '--handshake', action='store_true', help='Handshake')
+parser.add_argument('-gk', '--generate_key', action='store_true', help='Generate key')
+parser.add_argument('-ad','--access_data',  action='store_true', help='Access data')
+parser.add_argument('-s', '--slice', type=int, default=0)
+parser.add_argument('-r', '--reader', type=int, default=0)
+args = parser.parse_args()
+
+process_instance_id = config('PROCESS_ID') #Process id generated after the attribute certification
+slice_id = config('SLICE_ID_' + str(args.slice))
+message_id = config('MESSAGE_ID')
+reader_address = config('READER_' + str(args.reader))
+
+print("process_instance_id: {}".format(process_instance_id))
+print("slice_id: {}".format(slice_id))
+print("message_id: {}".format(message_id))
+print("reader_address: {}".format(reader_address))
+
+input = {'process_id' : process_instance_id,
+    'slice_id' : slice_id,
+    'message_id': message_id,
+    'reader_address' : reader_address}
+
+if args.handshake:
+    response = requests.post('http://127.0.0.1:8888/client/handshake',
+        json = input)
+if args.generate_key:
+    response = requests.post('http://127.0.0.1:8888/client/generateKey',
+        json = input)
+if args.access_data:
+    response = requests.post('http://127.0.0.1:8888/client/accessaData',
+        json = input)
+                
