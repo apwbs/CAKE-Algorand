dd8c7bd8d2504d39e9cb28fcf17ff2f624d75d92
Michele Spina <michelespina96@gmail.com>
2023-05-09 11:51:59 +0200

    fix account creation and add verbose attribut

--- account_creation.py
+++ account_creation.py
@@ -10,30 +10,37 @@ headers = {
    "X-API-Key": algod_token,
 }
 
-roles = ['CERTIFIER', 'MANUFACTURER', 'SUPPLIER1', 'SUPPLIER2', 'SKM', 'SDM']
-
+ACTORS = [ 'MANUFACTURER', 'SUPPLIER1', 'SUPPLIER2']
+MUSTHAVE = ['CERTIFIER', 'SKM', 'SDM']
 
 algod_client = algod.AlgodClient(algod_token, algod_address, headers)
 
 
-def generate_algorand_keypair(role = "My"):
+def generate_algorand_keypair(role = "My", verbose = True):
     private_key, address = account.generate_account()
-    print(role + " address: {}".format(address))
-    print(role +" private key: {}".format(private_key))
-    print(role +" passphrase: {}".format(mnemonic.from_private_key(private_key)))
-    print("\n")
+    if verbose:
+        print(role + " address: {}".format(address))
+        print(role +" private key: {}".format(private_key))
+        print(role +" passphrase: {}".format(mnemonic.from_private_key(private_key)))
+        print("\n")
     return address, private_key
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-A' ,'--all', action='store_true')
+parser.add_argument('-v' ,'--verbose', action='store_true')
 
 args = parser.parse_args()
 
 if args.all:
     if input("This operation will reset address and private key stored in .env, press y to continue or any other key to abort: ").lower() != 'y':
         exit()
-    for role in roles:
-        address, private_key =  generate_algorand_keypair(role + "'")
+    with open('.env', 'r', encoding='utf-8') as file:
+        data = file.readlines()
+    data.append("\n")
+    with open('.env', 'w', encoding='utf-8') as file:
+        file.writelines(data)
+    for role in ACTORS:
+        address, private_key =  generate_algorand_keypair(role + "'", args.verbose)
 
         with open('.env', 'r', encoding='utf-8') as file:
             data = file.readlines()
@@ -42,17 +49,39 @@ if args.all:
                 data.remove(line)
                 break
         for line in data:
-            if line.startswith(role + '_PRIVATEKEY'):
+            if line.startswith('PRIVATEKEY_' + role):
                 data.remove(line)
                 break
-        line = 'ADDRESS_' + role + "=" + address + "\n"
+        line = 'ADDRESS_' + role + "='" + address + "'\n"
         data.append(line)
-        line = role + '_PRIVATEKEY=' + private_key + "\n"
+        line = 'PRIVATEKEY_' + role + "='" + private_key + "'\n"
         data.append(line)
         with open('.env', 'w', encoding='utf-8') as file:
             file.writelines(data)
-    
-    address, private_key =  generate_algorand_keypair('CREATOR' + '\'s')
+
+    for role in MUSTHAVE:
+        address, private_key =  generate_algorand_keypair(role + "'", args.verbose)
+
+        with open('.env', 'r', encoding='utf-8') as file:
+            data = file.readlines()
+        for line in data:
+            if line.startswith(role + '_ADDRESS'):
+                data.remove(line)
+                break
+        for line in data:
+            if line.startswith(role + '_PRIVATEKEY'):
+                data.remove(line)
+                break
+        line = role + "_ADDRESS='" + address + "'\n"
+        data.append(line)
+        line = role + "_PRIVATEKEY='" + private_key + "'\n"
+        data.append(line)
+        with open('.env', 'w', encoding='utf-8') as file:
+            file.writelines(data)
+
+
+# Update CREATOR address and private key    
+    address, private_key =  generate_algorand_keypair('CREATOR' + '\'s', False)
     with open('.env', 'r', encoding='utf-8') as file:
         data = file.readlines()
     for line in data:
@@ -63,9 +92,9 @@ if args.all:
         if line.startswith('CREATOR_PRIVATEKEY'):
             data.remove(line)
             break
-    line = "ADDRESS_CREATOR=" + address + "\n"
+    line = "ADDRESS_CREATOR='" + address + "'\n"
     data.append(line)
-    line =  'CREATOR_PRIVATEKEY=' + private_key + "\n"
+    line =  "CREATOR_PRIVATEKEY='" + private_key + "'\n"
     data.append(line)
 
     passphrase = mnemonic.from_private_key(private_key)
@@ -73,10 +102,12 @@ if args.all:
         if line.startswith('PASSPHRASE_CREATOR'):
             data.remove(line)
             break
-    line = 'PASSPHRASE_CREATOR=' + passphrase + "\n"  
+    line = "PASSPHRASE_CREATOR='" + passphrase + "'\n"  
     data.append(line)
     with open('.env', 'w', encoding='utf-8') as file:
         file.writelines(data)
+
+    print("All addresses and private keys are updated in .env")
 
 else:
     generate_algorand_keypair()
